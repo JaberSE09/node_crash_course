d64fb2ffe8c7a695ab66ecd85b78dda9abdbca66
Sajid Jaber <sajidjaber@yahoo.com>
2022-04-08 17:05:52 -0500

    finished path basics

--- reference/path_demo.js
+++ reference/path_demo.js
@@ -6,4 +6,9 @@ console.log(path.basename(__filename))
 console.log(path.dirname(__filename))
 //file extension
 console.log(path.extname(__filename))
+//create path object
+console.log(path.parse(__filename))
+//join concat paths
+console.log(path.join(__dirname , "test" , "hello.html"))
+
 
