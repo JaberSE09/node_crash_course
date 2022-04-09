5fdf9643a26a9abb7dc05a1c47617803ab613c48
Sajid Jaber <sajidjaber@yahoo.com>
2022-04-08 19:08:22 -0500

    fs demo read file setup

--- reference/fs_demo.js
+++ reference/fs_demo.js
@@ -7,13 +7,22 @@ const path = require("path")
 // })
 
 //create and write file
-fs.writeFile(path.join(__dirname , "/test", "hello.txt") , "Hello World!" , function(err){
+fs.writeFile(path.join(__dirname, "/test", "hello.txt"), "Hello World!", function (err) {
     if (err) throw err
     console.log("Created File...")
-    })
+
 
     //Append to a file
-fs.appendFile(path.join(__dirname , "/test", "hello.txt") , " I love nodejs!" , function(err){
-    if (err) throw err
-    console.log("Append File...")
+    fs.appendFile(path.join(__dirname, "/test", "hello.txt"), " I love nodejs!", function (err) {
+        if (err) throw err
+        console.log("Append File...")
     })
+    //Read File
+    fs.readFile(path.join(__dirname, "/test", "hello.txt"), "utf-8", function (err, data) {
+        if (err) throw err
+        console.log(data)
+    })
+
+    
+})
+
