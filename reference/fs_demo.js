4371e38a3a2e86947203ba1dde7256b33dcf3464
Sajid Jaber <sajidjaber@yahoo.com>
2022-04-08 19:00:45 -0500

    fs demo create file and append file

--- reference/fs_demo.js
+++ reference/fs_demo.js
@@ -1,7 +1,19 @@
 const fs = require("fs")
 const path = require("path")
 
-fs.mkdir(path.join(__dirname , "/test") , {} , function(err){
-if (err) throw err
-console.log("Created File...")
-})
+// fs.mkdir(path.join(__dirname , "/test") , {} , function(err){
+// if (err) throw err
+// console.log("Created Folder...")
+// })
+
+//create and write file
+fs.writeFile(path.join(__dirname , "/test", "hello.txt") , "Hello World!" , function(err){
+    if (err) throw err
+    console.log("Created File...")
+    })
+
+    //Append to a file
+fs.appendFile(path.join(__dirname , "/test", "hello.txt") , " I love nodejs!" , function(err){
+    if (err) throw err
+    console.log("Append File...")
+    })
