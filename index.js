8faf0997aec4acb36f55b03b85caaf2727d48059
Sajid Jaber <sajidjaber@yahoo.com>
2022-04-10 18:24:04 -0500

    set up server

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+const http = require("http")
+const path = require("path")
+const fs = require("fs")
+
+const server = http.createServer((req, res) =>{
+
+    if (req.url == "/"){
+        res.end("<h1>HomePage</h1>")
+        res.writeHead(200 , {"Content-Type": "text/html"})
+    }
+} )
+
+PORT= process.env.PORT || 5000 
+
+server.listen(PORT, () => console.log(`Server Running on Port ${PORT}`))
 
