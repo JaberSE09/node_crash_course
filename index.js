4467ccc1a2596ba5a6bd0ed6fc3f6356d364e097
Sajid Jaber <sajidjaber@yahoo.com>
2022-04-10 23:21:32 -0500

    html displaying

--- index.js
+++ index.js
@@ -2,15 +2,29 @@ const http = require("http")
 const path = require("path")
 const fs = require("fs")
 
-const server = http.createServer((req, res) =>{
+const server = http.createServer((req, res) => {
 
-    if (req.url == "/"){
-        res.end("<h1>HomePage</h1>")
-        res.writeHead(200 , {"Content-Type": "text/html"})
+    if (req.url == "/") {
+
+        fs.readFile(path.join(__dirname, "public", "index.html"), (err, content) => {
+            res.end(content)
+            res.writeHead(200, { "Content-Type": "text/html" })
+            if (err) throw err
+        })
+
     }
-} )
+    if (req.url == "/about") {
 
-PORT= process.env.PORT || 5000 
+        fs.readFile(path.join(__dirname, "public", "about.html"), (err, content) => {
+            res.end(content)
+            res.writeHead(200, { "Content-Type": "text/html" })
+            if (err) throw err
+        })
+
+    }
+})
+
+PORT = process.env.PORT || 5000
 
 server.listen(PORT, () => console.log(`Server Running on Port ${PORT}`))
 
