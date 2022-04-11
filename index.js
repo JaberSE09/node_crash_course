1517b68f0011c493b30d610226d194c045b22b15
Sajid Jaber <sajidjaber@yahoo.com>
2022-04-10 23:54:39 -0500

    finished the basics

--- index.js
+++ index.js
@@ -1,37 +1,68 @@
-const http = require("http")
-const path = require("path")
-const fs = require("fs")
+const http = require("http");
+const path = require("path");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
+  //build file path
+  let filePath = path.join(
+    __dirname,
+    "public",
+    req.url == "/" ? "index.html" : req.url
+  );
 
-    if (req.url == "/") {
+  //Extension of File
+  let extname = path.extname(filePath);
 
-        fs.readFile(path.join(__dirname, "public", "index.html"), (err, content) => {
-            res.end(content)
-            res.writeHead(200, { "Content-Type": "text/html" })
-            if (err) throw err
-        })
+  //initial content type
+  let contentType = "text/html";
 
+  //check extention and set content type
+  switch (extname) {
+    case ".js":
+      contentType = "text/javascript";
+      break;
+    case ".css":
+      contentType = "text/css";
+      break;
+    case ".json":
+      contentType = "application/json";
+      break;
+    case ".png":
+      contentType = "image/png";
+      break;
+    case ".jpg":
+      contentType = "image/jpg";
+      break;
+  }
+
+  //read file
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      if (err.code == "ENOENT") {
+        // Page not found
+        fs.readFile(
+          path.join(__dirname, "public", "404.html"),
+          (err, content) => {
+            res.writeHead(404, { "Content-Type": "text/html" });
+            res.end(content, "utf8");
+          }
+        );
+      }
+      else{
+           //  Some server error
+        res.writeHead(500);
+        res.end(`Server Error: ${err.code}`);
+      }
+      }else {
+      // Success
+      res.writeHead(200, { "Content-Type": contentType });
+      res.end(content, "utf8");
     }
-    if (req.url == "/api/users") {
-
-        const users = [
-            {
-                name: "Bob Smith",
-                age: 40
-            },
-            {
-                name: "John Doe",
-                age: 30
-            }
-        ]
-
-        res.writeHead(200, {"Content-Type": "application/json"})
-        res.end(JSON.stringify(users))
     }
-})
+  
+  );
+});
 
-PORT = process.env.PORT || 5000
+PORT = process.env.PORT || 5000;
 
-server.listen(PORT, () => console.log(`Server Running on Port ${PORT}`))
-
+server.listen(PORT, () => console.log(`Server Running on Port ${PORT}`));
